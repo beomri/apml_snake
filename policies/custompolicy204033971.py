f6aaa03b45283210ba35e5577c4f994038076bcd
Franziska Weeber <f.weeber@cs.huji.ac.il>
2020-02-04 15:46:23 +0200

    go back to best working version and change get features function

--- policies/custompolicy204033971.py
+++ policies/custompolicy204033971.py
@@ -11,11 +11,13 @@ from tensorflow.keras.optimizers import Adam
 #tf.get_logger().setLevel('ERROR')  # don't print warnings (works in tf 2)
 
 
+
+
 EPSILON = 0.05
 LR = 0.001
 DISCOUNT = 0.15
 NUM_VALUES = 11
-STATE_DIM = 1 + NUM_VALUES + (5 * NUM_VALUES)
+STATE_DIM = (5 * NUM_VALUES)
 
 
 class Custom204033971(bp.Policy):
@@ -54,12 +56,12 @@ class Custom204033971(bp.Policy):
             self.log("Something Went Wrong...", 'EXCEPTION')
             self.log(e, 'EXCEPTION')
 
-        X = np.array(self.last_states)
-        Y = np.array([self.act_dict[a] for a in self.last_actions])
+        x_train = np.array(self.last_states)
+        y_train = np.array([self.act_dict[a] for a in self.last_actions])
 
-        SW = np.array(self.last_rewards) #*(DISCOUNT ** np.arange(len(self.last_rewards)))
+        sw = np.array(self.last_rewards) *(DISCOUNT ** np.arange(len(self.last_rewards)))
 
-        self.pn.train(X, Y, SW)
+        self.pn.train(x_train, y_train, sw)
 
         self.last_states = []
         self.last_actions = []
@@ -71,22 +73,18 @@ class Custom204033971(bp.Policy):
             self.last_states.append(self.get_features(prev_state))
             self.last_actions.append(prev_action)
             self.last_rewards.append(reward)
-        
+
         feats = self.get_features(new_state)
         weights = np.squeeze(self.pn.get_actions(feats))
         new_action = np.random.choice(bp.Policy.ACTIONS, p=weights)
         return new_action
-        
+
 
     def get_features(self, state):
-        temp_feats = np.zeros([6, NUM_VALUES])
+        temp_feats = np.zeros([8, NUM_VALUES])
 
         board, head = state
         head_pos, direction = head
-
-        r = head_pos[0]
-        c = head_pos[1]
-        temp_feats[0, board[r, c] + 1] = 1
 
         forward_region = ['F', 'F', 'F']
         forward_left_region = ['F', 'L', 'F', 'R', 'R', 'L', 'L']
@@ -104,10 +102,9 @@ class Custom204033971(bp.Policy):
                 temp_pos = temp_pos.move(bp.Policy.TURNS[direction][step])
                 r = temp_pos[0]
                 c = temp_pos[1]
-                temp_feats[route_ind + 1, board[r, c] + 1] += 1
+                temp_feats[route_ind, board[r, c] + 1] += 1
 
-        feats = np.ones(STATE_DIM)
-        feats[:-1] = temp_feats.flatten()
+        feats = temp_feats.flatten()
 
         return feats
 
@@ -116,37 +113,39 @@ class PolicyNetwork:
 
     def __init__(self, in_shape, out_shape, n_hidden_layers, n_nodes=64,
                  loss='sparse_categorical_crossentropy',
-                 optimizer=Adam, lr=0.0001, lr_decay=1e-6):
-
-
-        n_nodes = [n_nodes] * n_hidden_layers
+                 optimizer=Adam, lr=0.0001,):
 
         self.in_shape = in_shape
         self.out_shape = out_shape
         self.n_hidden_layers = n_hidden_layers
         self.n_nodes = n_nodes
         self.loss = loss
-        self.optimizer = optimizer(lr=lr, decay=lr_decay)
+        self.optimizer = optimizer(lr=lr)
         self.lr = lr
 
         input = Input(shape=(self.in_shape,))
         in_layer = input
         for i in range(n_hidden_layers):
-            out_layer = Dense(n_nodes[i], input_shape=(self.in_shape,),
-                              activation='relu')(in_layer)
+            out_layer = Dense(n_nodes, activation='relu')(in_layer)
             in_layer = out_layer
-        output = Dense(self.out_shape, input_shape=(self.n_nodes[-1],),
-                       activation='softmax')(out_layer)
+        output = Dense(self.out_shape, activation='softmax')(out_layer)
         model = Model(inputs=input, outputs=output)
         model.compile(loss=self.loss,
                       optimizer=self.optimizer,
                       metrics=['accuracy'])
 
+        n = 1
+        x_init = np.zeros(shape=(n, STATE_DIM))
+        y_init = np.random.randint(0, 3, n)
+
+        for i in range(n):
+            x_init[i] = np.random.randint(0, 6, size=STATE_DIM)
+        model.fit(x_init, y_init, verbose=False)
+        model.predict(x_init)
         self.model = model
-        
+
     def train(self, features, actions, rewards):
         self.model.fit(x=features, y=actions, sample_weight=rewards, verbose=False)
-        
+
     def get_actions(self, features):
         return self.model.predict(features[np.newaxis, :])
-
