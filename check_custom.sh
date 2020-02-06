49bc525435727e0bf078d505e0be1923170bd411
Omri Ben-Dov <omri.ben-dov@mail.huji.ac.il>
2020-02-06 14:15:17 +0200

    fix custom evaluation

--- check_custom.sh
+++ check_custom.sh
@@ -6,4 +6,4 @@
 #SBATCH --constraint="sm"
 
 module load tensorflow
-python3 Snake.py -pit 10 -D 50000 -s 5000 -pat 0.01 -plt 0.05 -P "Custom204033971();Avoid();Avoid()" 
+python3 Snake.py -pit 10 -D 50000 -s 5000 -pat 0.01 -plt 0.05 -P "Custom204033971();Avoid(epsilon=0.1);Avoid(epsilon=0.2);Avoid(epsilon=0.5);Avoid(epsilon=0)" 
