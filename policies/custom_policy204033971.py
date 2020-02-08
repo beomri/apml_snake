d7eb8c85ae27509a666fa2ba328d5c7bcc9349dd
Omri Ben-Dov <omri.ben-dov@mail.huji.ac.il>
2020-02-08 09:17:41 +0200

    change to old features

--- policies/custom_policy204033971.py
+++ policies/custom_policy204033971.py
@@ -3,30 +3,24 @@ import numpy as np
 from policies import base_policy as bp
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-#import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.optimizers import Adam
-from itertools import combinations_with_replacement
-#from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-
-#tf.get_logger().setLevel('ERROR')  # don't print warnings (works in tf 2)
 
 
 EPSILON = 0.05
 LR = 0.001
-DISCOUNT = 0.4
+DISCOUNT = 0.5
 NUM_VALUES = 11
 STATE_DIM = (16 * NUM_VALUES)
 HIDDEN = 2
 NODES = 32
-N_STEPS = 4
+
 
 class Custom204033971(bp.Policy):
     """
     A policy which avoids collisions with obstacles and other snakes.
-    It has an epsilon parameter which controls the
-    percentage of actions which are randomly chosen.
+    It uses gradient stochaditc policy.
     """
 
     def cast_string_args(self, policy_args):
@@ -46,25 +40,10 @@ class Custom204033971(bp.Policy):
                                 self.hidden,
                                 self.nodes,
                                 lr=self.lr)
-        self.act_dict = {a:n for n,a in enumerate(bp.Policy.ACTIONS)}
-        self.positions = self.create_positions()
+        self.act_dict = {a: n for n, a in enumerate(bp.Policy.ACTIONS)}
 
 
     def learn(self, round, prev_state, prev_action, reward, new_state, too_slow):
-
-        try:
-            if round % 100 == 0:
-                if round > self.game_duration - self.score_scope:
-                    self.log("Rewards in last 100 rounds which counts towards the score: " + str(self.r_sum), 'VALUE')
-                else:
-                    self.log("Rewards in last 100 rounds: " + str(self.r_sum), 'VALUE')
-                self.r_sum = 0
-            else:
-                self.r_sum += reward
-
-        except Exception as e:
-            self.log("Something Went Wrong...", 'EXCEPTION')
-            self.log(e, 'EXCEPTION')
 
         x_train = np.array(self.last_states)
         y_train = np.array([self.act_dict[a] for a in self.last_actions])
@@ -72,7 +51,6 @@ class Custom204033971(bp.Policy):
         sw = np.array(self.last_rewards)
         for it in range(2, len(sw)+1):
             sw[-it] += self.discount * sw[1 - it]
-        # sw = np.array(self.last_rewards) * (self.discount ** np.arange(len(self.last_rewards)))
 
         self.pn.train(x_train, y_train, sw)
 
@@ -87,7 +65,7 @@ class Custom204033971(bp.Policy):
             self.last_actions.append(prev_action)
             self.last_rewards.append(reward)
 
-        feats = self.get_step_features(new_state)
+        feats = self.get_features(new_state)
         weights = np.squeeze(self.pn.get_actions(feats))
         new_action = np.random.choice(bp.Policy.ACTIONS, p=weights)
         return new_action
@@ -111,24 +89,12 @@ class Custom204033971(bp.Policy):
         routes = [forward, left, right, forward_region, forward_left_region,
                  forward_right_region, right_region, left_region]
 
-#        for route_ind, route in enumerate(routes):
-#            temp_pos = head_pos
-#            temp_direction = bp.Policy.TURNS[direction][route[0]]
-#            temp_pos = temp_pos.move(temp_direction)
-#            for step in route[1:]:
-#                temp_direction = bp.Policy.TURNS[direction][step]
-#                temp_pos = temp_pos.move(temp_direction)
-#                r = temp_pos[0]
-#                c = temp_pos[1]
-#                temp_feats[route_ind, board[r, c] + 1] += 1
-                
         for route_ind, route in enumerate(routes):
             temp_pos = head_pos
             temp_direction = direction
             for step in route:
                 temp_direction = bp.Policy.TURNS[temp_direction][step]
                 temp_pos = temp_pos.move(temp_direction)
-#                temp_pos = temp_pos.move(bp.Policy.TURNS[direction][step])
                 r = temp_pos[0]
                 c = temp_pos[1]
                 temp_feats[route_ind, board[r, c] + 1] += 1
@@ -137,65 +103,6 @@ class Custom204033971(bp.Policy):
 
         return feats
 
-    def get_step_features(self, state):
-
-        temp_feats = np.zeros([4 * N_STEPS, NUM_VALUES])
-
-        board, head = state
-        head_pos, direction = head
-
-        max_x = board.shape[0]
-        max_y = board.shape[1]
-
-        for pos_ind, pos in enumerate(self.positions[direction].keys()):
-            for version in self.positions[direction][pos]:
-                r = int((head_pos[0] + version[0]) % max_x)
-                c = int((head_pos[1] + version[1]) % max_y)
-                temp_feats[pos_ind, board[r, c] + 1] += 1
-
-        feats = temp_feats.flatten()
-
-        return feats
-
-    def create_positions(self, n_steps=4):
-
-        moves = {'N_F': ('N', (0, 1)),
-                 'N_L': ('W', (-1, 0)),
-                 'N_R': ('E', (1, 0)),
-                 'E_F': ('E', (1, 0)),
-                 'E_L': ('N', (0, 1)),
-                 'E_R': ('S', (0, -1)),
-                 'S_F': ('S', (0, -1)),
-                 'S_L': ('E', (1, 0)),
-                 'S_R': ('W', (-1, 0)),
-                 'W_F': ('W', (-1, 0)),
-                 'W_L': ('S', (0, -1)),
-                 'W_R': ('N', (0, 1))}
-
-        positions = {}
-        for pos in bp.Policy.TURNS.keys():
-            positions[pos] = {}
-            for i in range(1, n_steps + 1):
-                end_states = []
-                combinations = combinations_with_replacement(bp.Policy.ACTIONS, i)
-                for comb in combinations:
-                    dir = pos
-                    x_moves = np.zeros(i)
-                    y_moves = np.zeros(i)
-                    for ind, act in enumerate(comb):
-                        move = moves[f'{dir}_{act}']
-                        dir = move[0]
-                        x_moves[ind] = move[1][0]
-                        y_moves[ind] = move[1][1]
-                    end_states.append((np.sum(x_moves), np.sum(y_moves)))
-
-                end_states = list(set(end_states))
-                positions[pos][f'{i}_l'] = [p for p in end_states if p[0] < 0 and abs(p[0]) >= abs(p[1])]
-                positions[pos][f'{i}_r'] = [p for p in end_states if p[0] > 0 and abs(p[0]) >= abs(p[1])]
-                positions[pos][f'{i}_d'] = [p for p in end_states if p[1] < 0 and abs(p[0]) <= abs(p[1])]
-                positions[pos][f'{i}_u'] = [p for p in end_states if p[1] > 0 and abs(p[0]) <= abs(p[1])]
-
-        return positions
 
 
 class PolicyNetwork:
